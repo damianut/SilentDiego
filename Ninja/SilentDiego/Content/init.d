4a6657359fdc4221b71fe1ac44a1855c722b8f1a
damianut <41262420+damianut@users.noreply.github.com>
2024-09-06 16:31:09 +0000

    Initialize patch

---
+++ Ninja/SilentDiego/Content/init.d
@@ -0,0 +1,9 @@
+/*
+ * Initialization function called by Ninja after "Init_Global" (G2) / "Init_<Levelname>" (G1)
+ */
+func void Ninja_SilentDiego_Init() {
+    LeGo_MergeFlags( /* DESIRED LEGO PACKAGES */ );
+
+    // WRITE YOUR INITIALIZATIONS HERE
+
+};
